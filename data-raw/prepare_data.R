0318edb9ec60796cfe3ac629050d7650a6afb67b
Darcy Webber <darcy@quantifish.co.nz>
2019-10-14 17:21:51 +1300

    update sticker

--- data-raw/prepare_data.R
+++ data-raw/prepare_data.R
@@ -25,7 +25,9 @@ p <- ggplot() +
   plot_coast(resolution = "low", fill = "orange", colour = NA, size = 0.3) +
   theme_void() + theme_transparent()
 
-sticker(p, package = "nzsf", h_color = "orange",
+sticker(p, package = "nzsf", 
+        h_color = "orange", h_fill = "#1881C2", 
+        #spotlight = TRUE, l_x = 0.8, l_y = 1,
         p_y = 1.65, p_size = 20, p_color = "orange",
         s_x = 1, s_y = 0.85, s_width = 1.3, s_height = 1.3, 
         filename = "sticker.png")
