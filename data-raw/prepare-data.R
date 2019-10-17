6ec2363b92c2558791cd361d382d62c88ae9038e
Darcy Webber <darcy@quantifish.co.nz>
2019-10-17 14:19:53 +1300

    new sticker script

--- data-raw/prepare-data.R
+++ data-raw/prepare-data.R
@@ -14,25 +14,6 @@ unzip_and_clean <- function(f) {
   return(x)
 }
 
-
-# Generate sticker
-
-# devtools::install_github("GuangchuangYu/hexSticker")
-library(hexSticker)
-library(nzsf)
-library(ggspatial)
-
-p <- ggplot() +
-  plot_coast(resolution = "low", fill = "orange", colour = NA, size = 0.3) +
-  theme_void() + theme_transparent()
-
-sticker(p, package = "nzsf", 
-        h_color = "orange", h_fill = "#1881C2", 
-        #spotlight = TRUE, l_x = 0.8, l_y = 1,
-        p_y = 1.65, p_size = 20, p_color = "orange",
-        s_x = 1, s_y = 0.85, s_width = 1.3, s_height = 1.3, 
-        filename = "sticker.png")
-
 # Various ---
 
 FisheriesManagementAreas <- unzip_and_clean("data-raw/FisheriesManagementAreas.zip") %>%
---
+++ data-raw/sticker.R
@@ -0,0 +1,18 @@
+# devtools::install_github("GuangchuangYu/hexSticker")
+library(hexSticker)
+library(nzsf)
+library(ggspatial)
+
+p <- ggplot() +
+  plot_coast(resolution = "low", fill = "orange", colour = NA, size = 0.3) +
+  theme_void() + 
+  theme_transparent()
+
+# Generate sticker
+
+sticker(p, package = "nzsf", 
+        h_color = "orange", h_fill = "#1881C2", 
+        #spotlight = TRUE, l_x = 0.8, l_y = 1,
+        p_y = 1.65, p_size = 20, p_color = "orange",
+        s_x = 1, s_y = 0.85, s_width = 1.3, s_height = 1.3, 
+        filename = "sticker.png")
