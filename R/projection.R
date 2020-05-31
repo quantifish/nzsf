7743858395f51ce89381122cce5469b0b19453d4
Darcy Webber <darcy@quantifish.co.nz>
2020-05-31 20:50:57 +1200

    adding ccamlr and ccsbt

--- R/projection.R
+++ R/projection.R
@@ -13,10 +13,12 @@ proj_nzsf <- function() {
 #' 
 #' This is the CRS 3031 with a 180 degree rotation.
 #'
+#' @param rotation The rotation to apply.
+#' 
 #' @export
 #' 
-proj_ccamlr <- function() {
-  "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=180 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+proj_ccamlr <- function(rotation = 180) {
+  paste0("+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=", rotation, " +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 }
 
 
