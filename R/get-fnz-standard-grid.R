42eb8866413dd58bc21d13030d052c2d519c6a7d
quantifish <darcy@quantifish.co.nz>
2021-06-14 23:04:16 +1200

    I had a go at this but got it wrong

--- R/get-fnz-standard-grid.R
+++ R/get-fnz-standard-grid.R
@@ -1,6 +1,6 @@
 #' Get Fisheries New Zealand standard grid origin
 #' 
-#' @param x square grid boundary length in km
+#' @param cell_size square grid boundary length in km
 #' @param bounding_box limits generated from call to sf::st_bbox()
 #' @return standard grid origin \code{data.frame}
 #'
@@ -9,15 +9,21 @@
 #' 
 #' @export
 #' 
-get_standard_grid_origin <- function(x, bounding_box) {
+get_standard_grid_origin <- function(cell_size, bounding_box) {
+  
+  cell_size_m <- cell_size * 1000
   
   bb_xmin <- as.numeric(bounding_box$xmin)
+  bb_xmax <- as.numeric(bounding_box$xmax)
   bb_ymin <- as.numeric(bounding_box$ymin)
+  bb_ymax <- as.numeric(bounding_box$ymax)
   
-  x <- data.frame(grid_size_km = x, 
-                  grid_size_km2 = x^2, 
-                  xmin = -ceiling(-bb_xmin / (x * 1000)) * (x * 1000), 
-                  ymin = -floor((-bb_ymin + 422600) / (x * 1000)) * (x * 1000) - 422600)
+  x <- data.frame(grid_size_km = cell_size, 
+                  grid_size_km2 = cell_size^2, 
+                  n_x = ceiling((bb_xmax - bb_xmin) / cell_size_m),
+                  n_y = ceiling((bb_ymax - bb_ymin) / cell_size_m),
+                  xmin = -ceiling(-bb_xmin / cell_size_m) * cell_size_m, 
+                  ymin = -floor((-bb_ymin + 422600) / cell_size_m) * cell_size_m - 422600)
   
   return(x)
 }
@@ -25,7 +31,7 @@ get_standard_grid_origin <- function(x, bounding_box) {
 
 #' Get Fisheries New Zealand standard grid definitions
 #' 
-#' @param x square grid boundary length in km
+#' @param cell_size square grid boundary length in km
 #' @param bounding_box limits generated from call to sf::st_bbox()
 #' @param clip_eez should the grid be clipped to the NZ EEZZ
 #' @return New Zealands standard grid polygon as a \code{sf} object.
@@ -36,9 +42,9 @@ get_standard_grid_origin <- function(x, bounding_box) {
 #' @importFrom sf st_make_grid st_intersection
 #' @export
 #' 
-get_standard_grid <- function(x, bounding_box, clip_eez = TRUE) {
+get_standard_grid <- function(cell_size, bounding_box, clip_eez = TRUE) {
   
-  grid_origin <- get_standard_grid_origin(x, bounding_box)
+  grid_origin <- get_standard_grid_origin(cell_size, bounding_box)
   
   grids <- bounding_box %>% 
     st_make_grid(cellsize = as.numeric(grid_origin["grid_size_km"]) * 1000, 
