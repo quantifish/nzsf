f8ebabcaa3f7c831c76a0d00593af4d4e6cce3db
quantifish <darcy@quantifish.co.nz>
2022-03-23 19:48:41 +1300

    edits to brms plots

--- R/plot-brms.R
+++ R/plot-brms.R
@@ -1,6 +1,8 @@
 #' Plot a conditional_smooths output
 #' 
 #' @inheritParams brms::conditional_smooths
+#' @param crs1 The CRS of the coordinates in the model.
+#' @param crs2 The desired output CRS.
 #' @param ... Other arguments passed on to \code{conditional_smooths}.
 #' @return a \code{data.frame}.
 #' @importFrom brms conditional_smooths
@@ -8,7 +10,7 @@
 #' @export
 #' 
 get_conditional_smooths <- function(x, resolution = 100, smooths = NULL, crs1 = st_crs(4326), crs2 = proj_nzsf(), ...) {
-  df <- conditional_smooths(x = x, resolution = resolution, smooths = smooths)[[1]] %>%
+  df <- conditional_smooths(x = x, resolution = resolution, smooths = smooths, ...)[[1]] %>%
     rename(x = 1, y = 2) %>%
     st_as_sf(coords = c("x", "y"), crs = crs1) %>%
     st_transform(crs = crs2)
@@ -27,6 +29,6 @@ get_conditional_smooths <- function(x, resolution = 100, smooths = NULL, crs1 = 
 #' 
 plot_conditional_smooths <- function(x, resolution = 100, smooths = NULL, crs1 = st_crs(4326), crs2 = proj_nzsf(), ...) {
   df <- get_conditional_smooths(x = x, resolution = resolution, smooths = smooths, crs1 = crs1, crs2 = crs2)
-  p <- plot_raster(data = df, field = "estimate__", fun = mean, nrow = resolution, ncol = resolution)
+  p <- plot_raster(data = df, field = "estimate__", fun = mean, nrow = resolution, ncol = resolution, ...)
   return(p)
 }
